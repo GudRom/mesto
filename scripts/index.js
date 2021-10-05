cb37af548e1734db37717ac340baccdbda2deab3
Roman Gudozhnikov <gudozhnikoff.rom@yandex.ru>
2021-10-05 22:55:08 +0300

    Сделать кнопки в JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,10 @@
 const popup = document.querySelector('.popup');
 const quitButton = document.querySelector('.popup__quit-button');
 const editButton = document.querySelector('.wanderer__edit-button');
-const form = document.querySelectorAll('popup__form-text')
+const formPopup = document.querySelector('.popup__form');
+const textInput = formPopup.querySelectorAll('.popup__form-text');
+const nameWanderer = document.querySelector('.wanderer__name');
+const jobWanderer = document.querySelector('.wanderer__subtitle');
 
 function popupOpen() {
     popup.classList.add('popup_active')
@@ -11,9 +14,16 @@ function popupClose() {
     popup.classList.remove('popup_active')
 }
 
+
 editButton.addEventListener('click', popupOpen);
+
 quitButton.addEventListener('click', popupClose);
-form.addEventListener('submit', function(event) {
-    event.preventDefault()
+
+function submitForm(event) {
+    event.preventDefault();
+    nameWanderer.textContent = textInput[0].value;
+    jobWanderer.textContent = textInput[1].value;
     popupClose();
-})
+}
+
+formPopup.addEventListener('submit', submitForm);
